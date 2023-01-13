2461572eeecf07775ef3f269a86c29da5f464c86
Otabek <turayevotabek.0002@gmail.com>
2023-01-13 17:26:59 +0500

    new js file

---
+++ intro/js/script.js
@@ -0,0 +1,13 @@
+let name = 'Otabek'
+let amount = 5
+let id = 12345678
+let animal = 'dog'
+let city = 'Karshi'
+
+console.log(name)
+console.log(amount)
+console.log(id)
+console.log(animal)
+console.log(city)
+
+// homework
